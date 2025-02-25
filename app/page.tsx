cdf1c9d0618da8d0bcc2d04d3c17b42a06d723aa
Philipp Isaak <phi.isaak@gmail.com>
2025-02-25 22:04:45 +0100

    social media

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Hero from "@/app/components/Hero";
 import {ArrowTopRightOnSquareIcon} from "@heroicons/react/24/outline";
+import {SocialIcon} from "react-social-icons/component";
+import 'react-social-icons/youtube';
+import 'react-social-icons/tiktok';
+import 'react-social-icons/instagram';
 
 export default function Home() {
     return (
@@ -7,7 +11,6 @@ export default function Home() {
             <main>
                 <Hero></Hero>
                 <div id="content">
-                    {/* Dummy Sections */}
                     <section id="concerts" className="py-20 lg:py-32">
                         <div className="container mx-auto text-center">
                             <h2 className="text-4xl lg:text-6xl text-primary mb-20">Upcoming Concerts</h2>
@@ -32,7 +35,7 @@ export default function Home() {
                                     </tr>
                                     <tr>
                                         <th>12.04.2025</th>
-                                        <td>d'Seiser Kurve</td>
+                                        <td>d&#39;Seiser Curve</td>
                                         <td>Sins, AG</td>
                                         <td><a className="btn btn-link" href="https://seiserkurve.ch/programm/adam-und-voltage-arc.html" target="_blank" rel="noopener noreferrer">More <ArrowTopRightOnSquareIcon className="h-4 w-4" /> </a></td>
                                     </tr>
@@ -42,11 +45,26 @@ export default function Home() {
 
                         </div>
                     </section>
-                    <section id="services" className="h-screen flex items-center justify-center bg-gray-200">
-                        <h2 className="text-3xl font-bold">Our Services</h2>
+                    <section id="booking" className="py-20 lg:py-32 bg-gray-200">
+                        <div className="container mx-auto text-center">
+                            <h2 className="text-4xl lg:text-6xl text-primary mb-20">Contact / Booking</h2>
+                            <p className="text-2xl">Contact us now for any booking related questions and more.</p>
+                            <p className="text-2xl mt-2">dudesofadam@gmail.com</p>
+                            <a href="mailto:dudesofadam@gmail.com" className="btn btn-primary btn-lg mt-12">Contact us now!</a>
+                        </div>
                     </section>
-                    <section id="contact" className="h-screen flex items-center justify-center bg-gray-300">
-                        <h2 className="text-3xl font-bold">Contact Us</h2>
+                    <section id="socialmedia" className="py-20 lg:py-32 bg-gray-300">
+                        <div className="container mx-auto text-center">
+                            <h2 className="text-4xl lg:text-6xl text-primary mb-20">Social media</h2>
+                            <p className="text-2xl">Checkout out all of our social media channels.</p>
+                            <p className="text-2xl mt-2">Don&#39;t forget to follow!</p>
+
+                            <div className="flex justify-center space-x-12 flex-wrap mt-10">
+                                <div><SocialIcon url="https://www.instagram.com/dudesofadam" target="_blank" rel="noopener noreferrer" /> </div>
+                                <div><SocialIcon url="https://www.youtube.com/@Dudes_of_Adam" target="_blank" rel="noopener noreferrer"/> </div>
+                                <div><SocialIcon url="https://www.tiktok.com/@dudesofadam" target="_blank" rel="noopener noreferrer"/> </div>
+                            </div>
+                        </div>
                     </section>
                 </div>
             </main>
