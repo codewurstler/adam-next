df0690a85a3bb2af89c56538e8f24a48985b46a6
Philipp Isaak <phi.isaak@gmail.com>
2025-03-02 11:47:01 +0100

    fix navbar

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -1,28 +1,26 @@
-import React from 'react';
 import Link from "next/link";
 import Image from "next/image";
 import {Bars3BottomLeftIcon} from "@heroicons/react/24/outline";
 
 function Navigation() {
     return (
-        <div>
-            <div className="navbar bg-base-100 fixed top-0 left-0 z-10 shadow-sm">
-                <div className="container">
-                    <div className="flex-1">
-                        <Link className="btn btn-ghost text-lg" href="/">
-                            <Image src="/adam_schwarz.svg" width="50" height="20" alt="logo" />
-                        </Link>
-                    </div>
-                    <div className="flex-none">
-                        <details className="dropdown dropdown-bottom dropdown-end">
-                            <summary className="btn btn-square"><Bars3BottomLeftIcon className="h-6 w-6"></Bars3BottomLeftIcon></summary>
-                            <ul className="menu dropdown-content bg-base-100 rounded-box z-1 min-w-[200px] p-2 shadow-sm mt-3">
-                                <li><Link href="/#concerts">Upcoming Concerts</Link></li>
-                                <li><Link href="/#booking">Contact / Booking</Link></li>
-                                <li><Link href="/#social-media">Social Media</Link></li>
-                            </ul>
-                        </details>
-                    </div>
+        <div className="navbar bg-base-100 fixed top-0 left-0 z-10 shadow-sm">
+            <div className="container flex">
+                <div className="flex-1">
+                    <Link className="btn btn-ghost text-lg" href="/">
+                        <Image src="/adam_schwarz.svg" width="50" height="20" alt="logo"/>
+                    </Link>
+                </div>
+                <div className="flex-none">
+                    <details className="dropdown dropdown-bottom dropdown-end">
+                        <summary className="btn btn-square btn-soft btn-primary"><Bars3BottomLeftIcon
+                            className="h-6 w-6"></Bars3BottomLeftIcon></summary>
+                        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 min-w-[200px] p-2 shadow-sm mt-3">
+                            <li><Link href="/#concerts">Upcoming Concerts</Link></li>
+                            <li><Link href="/#booking">Contact / Booking</Link></li>
+                            <li><Link href="/#social-media">Social Media</Link></li>
+                        </ul>
+                    </details>
                 </div>
             </div>
         </div>
