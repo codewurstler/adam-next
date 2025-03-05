2f9f8fa90ea94a4c08cbbf7b5ed6aab011575936
Philipp Isaak <phi.isaak@gmail.com>
2025-03-05 19:10:48 +0100

    make it work

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,47 +32,34 @@ export const metadata: Metadata = {
     },
 };
 
-export const hreflangTags = `
-    <link rel="alternate" href="https://adam-band.com/" hreflang="en" />
-    <link rel="alternate" href="https://adam-band.com/" hreflang="x-default" />
-`;
 
 export default function RootLayout({
-                                       children,
-                                   }: Readonly<{
-    children: React.ReactNode;
+  children,
+}: Readonly<{
+  children: React.ReactNode;
 }>) {
-    return (
-        <html lang="en" data-theme="cupcake">
-        <head>
-            <meta name="description" content="Adam ist eine Schweizer Rockband, die kraftvollen Alternative Rock liefert. Discover our latest songs, upcoming concerts, and official merchandise!" />
-            <meta name="keywords" content="Swiss Rock Band, Alternative Rock Schweiz, Schweizer Rockband, Rockmusik Schweiz" />
-            <meta property="og:title" content="Adam Band | Swiss Rock Band – Alternative Rock aus der Schweiz" />
-            <meta property="og:description" content="Adam ist eine Schweizer Rockband, die kraftvollen Alternative Rock liefert. Entdecke unsere neuesten Songs, bevorstehende Konzerte und offizielles Merchandise!" />
-            <meta property="og:image" content="https://adam-band.com/opengraph-image.jpg" />
-            <link rel="canonical" href="https://adam-band.com/" />
-            <div dangerouslySetInnerHTML={{ __html: hreflangTags }} />
-        </head>
-        <body className={`${raj.className} flex flex-col min-h-screen`}>
-        <main className="grow">{children}</main>
-        <div className="bg-neutral">
-            <div className="container">
-                <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-                    <aside>
-                        <div className="container"></div>
-                        <Image src="/adam_white.svg" alt="Logo" width="60" height="60" />
-                        <p>
-                            Adam (The Band!)
-                        </p>
-                    </aside>
-                    <nav>
-                        <p className="footer-title">Legal</p>
-                        <Link className="link link-hover" href="/imprint">Imprint</Link>
-                    </nav>
-                </footer>
-            </div>
+  return (
+    <html lang="en">
+    <body className={`${raj.className} flex flex-col min-h-screen`}>
+    <main className="grow">{children}</main>
+    <div className="bg-neutral">
+        <div className="container">
+            <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+                <aside>
+                    <div className="container"></div>
+                    <Image src="/adam_white.svg" alt="Logo" width="60" height="60" />
+                    <p>
+                        Adam (The Band!)
+                    </p>
+                </aside>
+                <nav>
+                    <p className="footer-title">Legal</p>
+                    <Link className="link link-hover" href="/imprint">Imprint</Link>
+                </nav>
+            </footer>
         </div>
-        </body>
-        </html>
-    );
+    </div>
+    </body>
+    </html>
+  );
 }
